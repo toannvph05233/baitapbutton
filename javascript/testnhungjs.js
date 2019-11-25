5382bc205d812ea5ae8c39c74c4d4913fa0daaa7
= <=>
2019-11-25 16:36:29 +0700

    javascrip/25/11/2019

---
+++ javascript/testnhungjs.js
@@ -0,0 +1,22 @@
+function _hello() {
+    alert("chao` con cc")
+    var xe_may = new Array();
+    xe_may[0]="winner";
+    xe_may[1]="honda";
+    document.write(xe_may)
+
+
+}
+var a=4,b=15,c=6
+if (a>b){
+    if (a>c){
+        alert("A max")
+    } else alert("C max")
+} else {
+    if (b>c){
+        alert(" B max")
+    } else alert("C max")
+}
+
+
+
