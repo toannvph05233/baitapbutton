891079aea496b2039cf70a929de2ab8fd9786db1
= <=>
2019-12-18 15:36:25 +0700

    pass2

--- trang_Web_Ban_Hang/js/jsMang.js
+++ trang_Web_Ban_Hang/js/jsMang.js
@@ -1,24 +1,45 @@
 let arr = [
-    ["images/banghe.jpeg", "Bàn ghễ phòng khách gỗ gõ đỏ", "Giá: 16.000.000₫", "Lượt xem: 194", "Bộ salon phòng khách, gỗ đủ món tay 12 ,chạm rồng rất tinh sảo,hoàn thiện phun sơn cao cấp ,chống công viền vênh, mối mọt, bảo hành 24 tháng"],
-    ["images/giuong_go.jpg", "Giường ngủ có ngăn kéo", "Giá: 3.500.000₫", "Lượt xem: 1906", "Trạng thái:Đặt hàng - Màu sản phẩm:tràm óc chó\n" +
-    "                Chất liệu:Chất liệu: Gỗ công nghiệp MDF công nghệ ván sàn chịu lực tốt, đã qua tẩm sấy chống mối mọt,\n" +
+    ["images/banghe.jpeg", "Bàn ghễ phòng khách gỗ gõ đỏ", "Giá: 16.000.000₫",
+        "Lượt xem: 194", "Bộ salon phòng khách, gỗ đủ món tay 12 ,chạm rồng rất" +
+    " tinh sảo,hoàn thiện phun sơn cao cấp ,chống công viền vênh, mối mọt, bảo " +
+    "hành 24 tháng"],
+
+
+    ["images/giuong_go.jpg", "Giường ngủ có ngăn kéo", "Giá: 3.500.000₫", "Lượt " +
+    "xem: 1906", "Trạng thái:Đặt hàng - Màu sản phẩm:tràm óc chó\n" +
+    "                Chất liệu:Chất liệu: Gỗ công nghiệp MDF công nghệ ván sàn chịu" +
+    " lực tốt, đã qua tẩm sấy chống mối mọt,\n" +
     "                không cong vênh.\n" +
     "                Kích thước:rộng:1m6 x dài: 2m\n" +
     "                Bảo hành:12 tháng"],
-    ["images/sofa-vang-ni-gia-re-3.jpg", "Sofa văng nỉ giá rẻ 1m8 thiết kế hiện đại tại Hà Nội", "Giá: 3.750.000₫", " Lượt xem: 196", "mang phong cách hiện đại với kiểu đệm rít múi\n" +
-    "                là một trong những mẫu sofa văng hot nhất hiện nay. Với phong cách hiện đại, màu sắc trẻ trung, chất\n" +
-    "                liệu tốt, bạn hoàn toàn có thể tin chọn. Việc của bạn là chọn màu ghế, còn lại cứ để chúng tôi\n" +
+
+
+    ["images/sofa-vang-ni-gia-re-3.jpg", "Sofa văng nỉ giá rẻ 1m8 thiết kế hiện đại tại" +
+    " Hà Nội", "Giá: 3.750.000₫", " Lượt xem: 196", "mang phong cách hiện đại với kiểu đệm rít múi\n" +
+    "                là một trong những mẫu sofa văng hot nhất hiện nay. Với phong cách " +
+    "hiện đại, màu sắc trẻ trung, chất\n" +
+    "                liệu tốt, bạn hoàn toàn có thể tin chọn. Việc của bạn là chọn màu ghế, " +
+    "còn lại cứ để chúng tôi\n" +
     "                lo."],
-    ["images/ban-an-1.jpg", "Bàn ăn 4 ghế mango màu vàng giá rẻ", "Giá: 2.900.000₫", "Lượt xem: 1906", "Chất liệu bàn ăn mango 4 chỗ màu vàng là Gỗ nhập khẩu\n" +
-    "                Kích thước: dài: 1m2 – rộng 80 cm\n" +
-    "                Màu sắc: vàng\n" +
-    "                Đóng gói trong thùng carton, dễ dàng vận chuyển, không lo trầy xước.\n" +
-    "                Hàng luôn có sẵn, giao trong ngày, thanh toán khi nhận hàng và kiểm tra hàng."],
-    ["images/ban-trang-diem-1.jpg", "Bàn trang điểm giá rẻ hộc sâu –tp 01", "Giá: 1.800.000₫", "Lượt xem: 106", "Chất liệu bàn trang điểm giá rẻ hộc sâu là mặt đệm,gỗ\n" +
-    "                Kích thước: Rộng – sâu – cao\n" +
-    "                Màu sắc: trắng\n" +
-    "                Độ mới 100% chưa qua sử dụng."],
-    ["images/ban-giam-doc.jpg", "Bàn giám đốc 1m8 –tp 20", "Giá: 1.950.000₫", "Lượt xem: 176", "Bàn giám đốc 1m8 tại Hà Nội được làm bằng chất liệu gỗ công nghiệp cao cấp. Bàn được phủ một lớp\n" +
+
+
+    ["images/ban-an-1.jpg", "Bàn ăn 4 ghế mango màu vàng giá rẻ", "Giá: 2.900.000₫", "Lượt xem: 1906",
+        "Chất liệu bàn ăn mango 4 chỗ màu vàng là Gỗ nhập khẩu\n" +
+        "                Kích thước: dài: 1m2 – rộng 80 cm\n" +
+        "                Màu sắc: vàng\n" +
+        "                Đóng gói trong thùng carton, dễ dàng vận chuyển, không lo trầy xước.\n" +
+        "                Hàng luôn có sẵn, giao trong ngày, thanh toán khi nhận hàng và kiểm tra hàng."],
+
+
+    ["images/ban-trang-diem-1.jpg", "Bàn trang điểm giá rẻ hộc sâu –tp 01", "Giá: 1.800.000₫", "Lượt xem: 106",
+        "Chất liệu bàn trang điểm giá rẻ hộc sâu là mặt đệm,gỗ\n" +
+        "                Kích thước: Rộng – sâu – cao\n" +
+        "                Màu sắc: trắng\n" +
+        "                Độ mới 100% chưa qua sử dụng."],
+
+
+    ["images/ban-giam-doc.jpg", "Bàn giám đốc 1m8 –tp 20", "Giá: 1.950.000₫", "Lượt xem: 176", "Bàn giám đốc" +
+    " 1m8 tại Hà Nội được làm bằng chất liệu gỗ công nghiệp cao cấp. Bàn được phủ một lớp\n" +
     "                Melamine, sơn PU chống trầy xước tốt, ẩm mốc, bóng đẹp theo thời gian. Với độ dày 80mm, mặt bàn cong\n" +
     "                phía trước và có một khoảng trang trí độc đáo, cách điệu."]
 ]
@@ -85,12 +106,10 @@ function delete_arr() {
 
 function tim_kiem_vi_tri() {
     let timkiem = document.getElementById("btt_search").value;
-    alert(timkiem);
-    for (let i = 0; i < arr.length ; i++) {
+    for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < 5; j++) {
             if (arr[i][j] == timkiem) {
-                return i;
-                break;
+                return i + 1;
             }
         }
         return false;
@@ -98,20 +117,22 @@ function tim_kiem_vi_tri() {
 }
 
 function hienthimangtimkiem(a) {
-    let str = "<ul>";
-    str += "<li>";
-    str += "<img src=" + arr[a][0] + " height=\"150\" width=\"170\">"
+    let str1 = "<ul>"
+        + "<li>"
+        + "<img src=" + arr[a][0] + " height=\"150\" width=\"170\">"
         + "<h2>" + arr[a][1] + "</h2>" + arr[a][2] + "<br><br>" + arr[a][3] + "<br><br>"
-        + arr[a][4] + "<br><br>";
-    str += "</li>";
-    return str;
+        + arr[a][4] + "<br><br>"
+        + "</li>"
+    return str1;
 }
 
 function tim_kiem() {
     let timkiem = tim_kiem_vi_tri();
-    if (timkiem != false){
-        document.getElementById("hienthi").innerHTML = hienthimangtimkiem(timkiem);
-    }else {
+    alert(timkiem);
+    if (timkiem != false) {
+        alert(timkiem);
+        document.getElementById("hienthi").innerHTML = hienthimangtimkiem(parseInt(timkiem) - 1);
+    } else {
         alert("không tìm kiếm được! ")
     }
 }
